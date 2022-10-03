0ff2263bb6fa8620984ba55119c4547eea1c1c20
Eliza Carter <eliza.carter1996@gmail.com>
2022-10-03 13:12:36 -0400

    Task 7

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -59,7 +59,7 @@ print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon
 
 #%%
 
-## Task 6
+## Task 7.2
 #Create a variable pointing to the data file
 file_name = "./data/raw/sara.txt"
 
@@ -75,6 +75,8 @@ file_object.close()
 date_dict = {}
 location_dict = {}
 
+# Ask the user for a date, specifying the format
+user_date = input("Enter a date (M/D/YYYY):")
 
 #Pretend we read one line of data from the file
 for lineString in line_list:
@@ -96,6 +98,21 @@ for lineString in line_list:
     if obs_lc in ("1","2","3"):
         date_dict[record_id] = obs_date
         location_dict[record_id] = (obs_lat, obs_lon)
+        
+#Create an empty key list
+matching_keys = []
+    
+# Loop through all key, value pairs in the date_dictionary
+for the_key, the_value in date_dict.items():
+    #See if the date (the value) matches the user date
+    if the_value == user_date:
+        matching_keys.append(the_key)
+
+
+#Reveal locations for each key in matching_keys
+for matching_key in matching_keys:
+    obs_lat, obs_lon = location_dict[matching_key]
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}")
     
     #Print the location of sara
     #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
